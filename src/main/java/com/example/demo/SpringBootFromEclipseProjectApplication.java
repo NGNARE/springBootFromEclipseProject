f62c5887743b524ca067505d938ebf4e187ed4e4
ngnare_user <ngnareh@yahoo.fr>
2020-11-26 00:50:23 +0100

    1er commit Structure projet

---
+++ src/main/java/com/example/demo/SpringBootFromEclipseProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFromEclipseProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFromEclipseProjectApplication.class, args);
+	}
+
+}
